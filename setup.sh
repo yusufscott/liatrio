77150f8a4a7d02e46c28ad1a1585d1afe5f2130c
Yusuf Scott <yusufscott01@gmail.com>
2023-04-27 02:43:35 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -11,4 +11,5 @@ helm install app ./app
 export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=app,app.kubernetes.io/instance=app" -o jsonpath="{.items[0].metadata.name}")
 export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
 echo "Visit http://127.0.0.1:8080 to use your application"
+sleep 15
 kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
